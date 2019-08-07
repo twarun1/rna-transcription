50c7f1dc65603638b3cd7db696fd3d8858890958
arun <arun.dcrust+tw1@gmail.com>
2019-08-07 18:56:14 +0530

    [Arun/Arun] Fix. Solution

--- src/main/java/RnaTranscription.java
+++ src/main/java/RnaTranscription.java
@@ -1,7 +1,22 @@
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        StringBuffer tempBuffer=new StringBuffer(dnaStrand.length());
+        tempBuffer.append(dnaStrand);
+
+        for(int i=0;i<tempBuffer.length();i++)
+        {
+            if(tempBuffer.charAt(i)=='G')
+                tempBuffer.setCharAt(i,'C');
+            else if(tempBuffer.charAt(i)=='C')
+                tempBuffer.setCharAt(i,'G');
+            else if(tempBuffer.charAt(i)=='T')
+                tempBuffer.setCharAt(i,'A');
+            else
+                tempBuffer.setCharAt(i,'U');
+        }
+        return tempBuffer.toString();
+
     }
 
 }
